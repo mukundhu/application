566e04d143f6a3f103a197648488b169cc1d506c
mukundhu <30518745+mukundhu@users.noreply.github.com>
2017-08-31 15:55:25 +0530

    Update cleancorpus.R

--- R/cleancorpus.R
+++ R/cleancorpus.R
@@ -7,5 +7,6 @@ clean_corpus<-function(text_data){
   corpus<- tm_map(corpus, tolower)
   corpus<- tm_map(corpus, removeWords, stopwords("english"))
   corpus<- tm_map(corpus, stripWhitespace)
+  corpus<- tm_map(corpus, PlainTextDocument)
   return(corpus)
 }
