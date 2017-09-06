70f2ca0383806d0ec1147db49879e19989412f03
mukundhu <30518745+mukundhu@users.noreply.github.com>
2017-09-06 18:22:43 +0530

    Update matching.R

--- R/matching.R
+++ R/matching.R
@@ -2,6 +2,10 @@ matching<-function(finalkey){
 
   a<-as.vector(NULL)
   b<-as.vector(NULL)
+  library(readr)
+  
+  data <-- read_csv(readr_example("datafile.csv"))
+
  print("you are Selected")
   
   }
