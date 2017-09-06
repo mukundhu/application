b9747e3ee93cff0622be5de50ed36fca5849aef9
mukundhu <30518745+mukundhu@users.noreply.github.com>
2017-09-06 15:22:42 +0530

    Update matching.R

--- R/matching.R
+++ R/matching.R
@@ -2,7 +2,7 @@ matching<-function(finalkey){
 
   a<-as.vector(NULL)
   b<-as.vector(NULL)
-  data <-read.csv(file='R/datafile.csv')
+  data <-read.csv(file='datafile.csv')
    a <- finalkey
   return(a)
   
