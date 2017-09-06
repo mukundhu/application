9c91c037e92e23dea0dc1527b0cec438d1834446
mukundhu <30518745+mukundhu@users.noreply.github.com>
2017-09-06 19:27:33 +0530

    Update matching.R

--- R/matching.R
+++ R/matching.R
@@ -2,9 +2,11 @@ matching<-function(finalkey){
 
   a<-as.vector(NULL)
   b<-as.vector(NULL)
-  library(readr)
+  library(RCurl)
+  myfile <- getURL('https://raw.githubusercontent.com/mukundhu/application/master/data/datafile.csv', ssl.verifyhost=FALSE, ssl.verifypeer=FALSE)
+  datafile<- read.csv(textConnection(myfile), header=T)
  
- print(getwd())
+  print(datafile)
   
   }
 
