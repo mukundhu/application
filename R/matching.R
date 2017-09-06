03fa588c7972dfe3e08d444cc54540808643f736
mukundhu <30518745+mukundhu@users.noreply.github.com>
2017-09-06 19:14:47 +0530

    Update matching.R

--- R/matching.R
+++ R/matching.R
@@ -3,7 +3,12 @@ matching<-function(finalkey){
   a<-as.vector(NULL)
   b<-as.vector(NULL)
   library(readr)
-  csv.reader('datafile.csv', 'data/datafile.csv', 'datafile')
+  path <-"datafile.csv"
+  if (is.null(path)) {
+    dir(system.file("data", package = "readr"))
+  } else {
+    system.file("data", path, package = "readr", mustWork = TRUE)
+  }
  print("you are Selected")
   
   }
