4c44e66428411ab2d723d12c11b5f60896bed59b
mukundhu <30518745+mukundhu@users.noreply.github.com>
2017-09-06 15:02:50 +0530

    Update matching.R

--- R/matching.R
+++ R/matching.R
@@ -3,71 +3,9 @@ matching<-function(finalkey){
   a<-as.vector(NULL)
   b<-as.vector(NULL)
   datafile<-read.csv("datafile.csv")
-
-  for(i in 1:length(finalkey)){
-    a[i]<-pmatch(finalkey[i],datafile$keyword,0.5)
-    if(a[i]==0){
-      a[i]<-NA
-    }
+   a <- "sucessfull"
+  return(a)
+  
   }
 
-  if(all(is.na(a))){
-    again <- againtext()
-    finalkey<-final_key(again)
-    matching(finalkey)
-
-  }else{
-    a_table<-table(a)
-    tab<-as.integer(names(a_table))
-    index_match<-tab[which.min(datafile$Rank[tab])]
-    if(datafile$sub[index_match]==""){
-
-      print(paste0("you have selected ",datafile$service[index_match]," services ",datafile$sub.service[index_match]),max.levels=0)
-      x<-1
-      while(x==1){
-        confirm<-conf()
-        if(tolower(confirm)=="no"){
-          x<-0
-          again <- againtext()
-          finalkey<-final_key(again)
-          matching(finalkey)
-        }else{
-          x<-0
-          if(tolower(confirm)=="yes"){
-            print("your services are forwared to the department. Thank you for using our services")
-
-          }
-          else{
-            x<-1
-
-          }
-        }
-        x<-as.numeric(0)
-      }
-    }else{
-      print(paste0("Are you looking for services in ",datafile$keyword[index_match]),max.levels=0)
-      y<-1
-      while(y==1){
-        confirm<-conf()
-        if(tolower(confirm)=="no"){
-          y<-0
-          again <- againtext()
-          finalkey<-final_key()
-          matching(finalkey)
-        }else{
-          if(tolower(confirm)=="yes"){
-            y<-0
-            print(paste0("please select from ",datafile$sub[index_match]),max.levels=0)
-
-            again <- againtext()
-            finalkey<-final_key(again)
-            matching(finalkey)
-          }else{
-            y<-1
-          }
-        }
-      }
-      y<-as.numeric(0)
-    }
-  }
-}
+ 
