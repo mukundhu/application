26d952e4e19a9f8a3e24989503ae3561cca3ff1d
mukundhu <30518745+mukundhu@users.noreply.github.com>
2017-09-06 19:34:17 +0530

    Update matching.R

--- R/matching.R
+++ R/matching.R
@@ -23,8 +23,27 @@ matching<-function(finalkey){
     index_match<-tab[which.min(datafile$Rank[tab])]
     if(datafile$sub[index_match]==""){
 
-      print("sucessful")
+     for(i in 1:length(finalkey)){
+    a[i]<-pmatch(finalkey[i],datafile$keyword,0.5)
+    if(a[i]==0){
+      a[i]<-NA
+    }
+  }
+
+  if(all(is.na(a))){
+    again <- againtext()
+    finalkey<-final_key(again)
+    matching(finalkey)
+
+  }else{
+    a_table<-table(a)
+    tab<-as.integer(names(a_table))
+    index_match<-tab[which.min(datafile$Rank[tab])]
+    if(datafile$sub[index_match]==""){
+
+      print(paste0("you have selected ",datafile$service[index_match]," services ",datafile$sub.service[index_match]),max.levels=0)
   
   }
 
  }
+}
