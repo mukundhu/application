e8b6558609167262799a8406df74a1b001958819
mukundhu <30518745+mukundhu@users.noreply.github.com>
2017-09-06 19:31:17 +0530

    Update matching.R

--- R/matching.R
+++ R/matching.R
@@ -5,9 +5,26 @@ matching<-function(finalkey){
   library(RCurl)
   myfile <- getURL('https://raw.githubusercontent.com/mukundhu/application/master/data/datafile.csv', ssl.verifyhost=FALSE, ssl.verifypeer=FALSE)
   datafile<- read.csv(textConnection(myfile), header=T)
- 
-  print("sucessful")
+  for(i in 1:length(finalkey)){
+    a[i]<-pmatch(finalkey[i],datafile$keyword,0.5)
+    if(a[i]==0){
+      a[i]<-NA
+    }
+  }
+
+  if(all(is.na(a))){
+    again <- againtext()
+    finalkey<-final_key(again)
+    matching(finalkey)
+
+  }else{
+    a_table<-table(a)
+    tab<-as.integer(names(a_table))
+    index_match<-tab[which.min(datafile$Rank[tab])]
+    if(datafile$sub[index_match]==""){
+
+      print(paste0("you have selected ",datafile$service[index_match]," services ",datafile$sub.service[index_match]),max.levels=0)
   
   }
 
- 
+ }
