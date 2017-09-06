df70037041eddd46f904b5755b13b3ab88baa600
mukundhu <30518745+mukundhu@users.noreply.github.com>
2017-09-06 19:16:42 +0530

    Update matching.R

--- R/matching.R
+++ R/matching.R
@@ -3,13 +3,8 @@ matching<-function(finalkey){
   a<-as.vector(NULL)
   b<-as.vector(NULL)
   library(readr)
-  path <-"datafile.csv"
-  if (is.null(path)) {
-    dir(system.file("data", package = "readr"))
-  } else {
-    system.file("data", path, package = "readr", mustWork = TRUE)
-  }
- print("you are Selected")
+ 
+ print(getwd())
   
   }
 
